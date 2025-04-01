22bdd0816bd37309c4a3dc982b9320f4c8320cd3
mattivityroom <mattivityroom@gmail.com>
2025-04-01 09:47:37 +0700

    Days 3 button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.write("Hello World!")
+st.header('button')
+
+if st.button('Say hello', key='welcoming', use_container_width=True, type="primary"):
+    st.write('Well, hello there')
+else:
+    st.write('Goodbye')
+
+if st.button("Drop me", key='droping'):
+    st.write("Dropped")
+else:
+    st.write("Not Dropped")
