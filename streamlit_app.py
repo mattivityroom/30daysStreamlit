b41472d02173b23ef2d6ecad945251286c94d12e
mattivityroom <mattivityroom@gmail.com>
2025-04-02 12:32:33 +0700

    days 9

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,13 @@
 import streamlit as s
-
+import pandas as p
+import numpy as n
 from datetime import time, datetime
 
-s.header("s.Slider")
+s.header('Line Chart')
 
-s.subheader('Slider')
-
-age = s.slider('Ages?', 0, 100, 25)
-s.write(f"I'm in {age} old")
-
-s.subheader('Range slider')
-
-values = s.slider(
-     'Select a range of values',0, 10)
-s.write('Values:', values)
-
-s.subheader('Range time slider')
-
-appointment = s.slider(
-    'Schedule appointment',
-    value=(time(11,30), time(12,45))
+chart_data = p.DataFrame(
+    n.random.rand(3,3),
+    columns=['a','b','c']
 )
-s.write(f"Your appoinment {appointment}")
 
-s.subheader('Datetime slider')
-
-start_time = s.slider(
-     "When do you start?",
-     value=datetime(2025, 12, 1, 0, 0 ),
-     format="DD/MM/YY - hh:mm")
-s.write("Start time:", start_time)
+s.line_chart(chart_data)
